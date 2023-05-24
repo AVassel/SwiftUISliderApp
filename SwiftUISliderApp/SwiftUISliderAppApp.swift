4bb3ab68577e418f66bca0b1f7b17024f4f93885
A.Vassel <102523970+AVassel@users.noreply.github.com>
2023-05-24 16:19:57 +0300

    Initial Commit

---
+++ SwiftUISliderApp/SwiftUISliderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISliderAppApp.swift
+//  SwiftUISliderApp
+//
+//  Created by Anton Vassel on 24.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISliderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
